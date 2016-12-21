c9e2f675e707643044f9890bbd84ea3953c8dc98
trreitz <trreitz@gmail.com>
2016-12-21 17:17:19 -0500

    Add files via upload

---
+++ workout_loop.py
@@ -0,0 +1,23 @@
+workoutlist = [{
+"name": ['Murph'],
+"ex1": ["1 mile run"],
+"ex2": ['100 Air Squats'],
+"ex3": ['1 mile run'],
+"equip": ['Pull-up bar'],
+"body zone": ['arms', 'legs'],
+"aerobic score": int(10)
+},
+
+{"name": ['Cindy'],
+"ex1": ["15 Pull-ups"],
+"ex2": ['100 Air Squats'],
+"ex3": ['9 Pushups'],
+"equip": ['Pull-up bar'],
+"body zone": ['arms', 'legs'],
+"aerobic score": int(7)
+}
+]
+# I want to loop through the workouts in the workoutlist dictonary and print the name of the workout score that fits the if statement. 
+#for workout in workoutlist:
+  #      if (workoutlist['aerobic score']) > int(6):
+   #         print(workoutlist['name'])
